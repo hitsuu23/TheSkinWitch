72f93c41dcb0c465f5e57703fa6a43802bf21d31
GHOSTMISHU <137277320+hitsuu23@users.noreply.github.com>
2024-07-12 22:44:52 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,36 @@
 let slideIndex = 0;
-showSlides();
+let slides = document.getElementsByClassName("mySlides");
+let dots = document.getElementsByClassName("dot");
+let slideInterval;
 
 function showSlides() {
-    let slides = document.getElementsByClassName("mySlides");
     for (let i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";  
     }
     slideIndex++;
     if (slideIndex > slides.length) {slideIndex = 1}    
+    for (let i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
     slides[slideIndex-1].style.display = "block";  
-    setTimeout(showSlides, 2000); // Change image every 2 seconds
+    dots[slideIndex-1].className += " active";
+    slideInterval = setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
+
+function currentSlide(n) {
+    clearTimeout(slideInterval);
+    slideIndex = n - 1;
+    showSlides();
+}
+
+function plusSlides(n) {
+    clearTimeout(slideInterval);
+    slideIndex += n - 1;
+    if (slideIndex >= slides.length) {slideIndex = 0}
+    if (slideIndex < 0) {slideIndex = slides.length - 1}
+    showSlides();
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    showSlides();
+});
